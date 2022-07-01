63c78179eac07c9d9c9563c990a24c71c407c040
EC2 Default User <ec2-user@ip-172-31-5-76.ec2.internal>
2022-07-01 14:29:03 +0000

    Second commit

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,5 @@
 class FavoritesController < ApplicationController
-    
+
   def create
     @book = Book.find(params[:book_id])
     @favorite = current_user.favorites.create(book_id: params[:book_id])
